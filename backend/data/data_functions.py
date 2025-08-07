cf1e7c74c7ce083b0dae534fec91788489ef3648
FedeGalli <federicogalli96@gmail.com>
2025-08-07 22:36:27 +0200

    Code refactoring and trends implementation

--- backend/data/data_functions.py
+++ backend/data/data_functions.py
@@ -1,27 +1,24 @@
-from posix import stat
-from turtledemo.forest import start
 import polars as pl
-from data_aggregator import get_volumes_market_size_df, get_volumes_df, get_prices_df
+from data_aggregator import get_volume_market_size_df, get_volume_df, get_price_df, get_price_current_year, get_price_starting_year, get_volume_current_year, get_volume_starting_year
 
 class DataManager:
 
     def __init__(self):
-        self.start_year = 2018
-        self.volumes_data = get_volumes_df()
-        self.prices_data = get_prices_df()
-        self.market_size_data = get_volumes_market_size_df()
+        self.price_start_year = get_price_starting_year()
+        self.price_current_year = get_price_current_year()
+        self.volume_start_year = get_volume_starting_year()
+        self.volume_current_year = get_volume_current_year()
+        self.volumes_data = get_volume_df()
+        self.prices_data = get_price_df()
+        self.market_size_data = get_volume_market_size_df()
 
-    def get_volumes_market_size(self, com: str):
-        return self.market_size_data.filter(pl.col("DES_COMUNE") == com).select("TAGLIA_MERCATO")
-
-    def _get_volumes_filtered(self, com: str, year:str):
+    def _get_volume_filtered(self, com: str, year:str):
         return self.volumes_data.filter((pl.col("DES_COMUNE") == com) & (pl.col("ANNO") == year))
 
     def _get_prices_filtered(self, com: str, month_year:str):
         return self.prices_data.filter((pl.col("DES_COMUNE") == com) & (pl.col("MESE_ANNO") == month_year))
 
-    def get_volume_mq(self, com: str, mq:int, year:str):
-
+    def get_current_volume_mq(self, com: str, mq:int):
         # Retrieving columns name with number in the text
         allowed_mq_columns = [f for f in self.volumes_data.columns if "<" in f or ">" in f]
         mq_bounds = [int(f[1:-3]) for f in allowed_mq_columns]
@@ -32,7 +29,7 @@ class DataManager:
                 selected_mq_col = col
                 break
 
-        return self._get_volumes_filtered(com, year).select(selected_mq_col)
+        return self._get_volume_filtered(com, str(self.volume_current_year)).select(selected_mq_col)
 
     def get_min_price(self, com:str, month_year:str, zone:str, type: str, state:str):
         return self._get_prices_filtered(com, month_year).filter(
@@ -47,14 +44,35 @@ class DataManager:
             (pl.col("DES_STATO") == state) &
             (pl.col("DES_TIPOLOGIA") == type)
         ).select("COSTO_MAX")
-    def get_price_trend(self):
-        pass
-    def get_volume_trend(self, com: str, year:str):
-        trend_df = self._get_volumes_filtered(com, str(self.start_year))
-        for i in range(2024 - self.start_year):
-            trend_df = pl.concat([self._get_volumes_filtered(com, str(self.start_year + i + 1)), trend_df], how="vertical")
+
+    def get_price_trend(self, com: str):
+        trend_df = self._get_prices_filtered(com, str(self.price_start_year))
+        for i in range((self.price_current_year - self.price_start_year) * 2):
+            trend_df = pl.concat(
+                [
+                    # Building the semester/year with variable i
+                    self._get_prices_filtered(com, "0" + str(((i % 2) * 5) + 1) + "/" + str(self.price_start_year + int(i / 2) + 1)),
+                    trend_df
+                ], how="vertical"
+            )
 
         return trend_df
 
+    def get_volume_trend_mq(self, com: str):
+        trend_df = self._get_volume_filtered(com, str(self.volume_start_year))
+        for i in range(self.volume_current_year - self.volume_start_year):
+            trend_df = pl.concat([self._get_volume_filtered(com, str(self.volume_start_year + i + 1)), trend_df], how="vertical")
+
+        return trend_df
+
+    def get_volume_market_size(self, com: str):
+        return self.market_size_data.filter(pl.col("DES_COMUNE") == com).select("TAGLIA_MERCATO")
+
 d = DataManager()
-print(d.get_volume_trend("RONCADELLE", "2024"))
+print(d.get_current_volume_mq("BRESCIA", 100))
+print(d.get_price_trend("BRESCIA"))
+
+
+# Make the tends to be more grupped.
+# For volume only expose only the desired column MQ
+# For price expose only the selected zone, type and state
